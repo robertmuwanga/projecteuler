dbd40f29bbb7462742d18e0be56c7e37a6f57854
ugjim2k <ugjim2k@gmail.com>
2016-09-15 17:54:24 +0300

    Problem 5 is a work in progress...

--- Prob5.R
+++ Prob5.R
@@ -1,7 +1,20 @@
-x <- 1:20
-n <- 1
+### 
+# Problem 5
+# -------------------
+#
+# 2520 is the smallest number that can be divided by each of the 
+# numbers from 1 to 10 without any remainder.
+# 
+# What is the smallest positive number that is evenly divisible
+# by all of the numbers from 1 to 20?
+###
 
-for(elem in x) {
-    n <- c(n, elem)
-    x <<- x %/% elem
+rj <- function() {
+    x <- 1:20
+    
+    getrange <- function() x
+    
+    updateRange <- function(startIndex, n) {
+        x <- c(x[1:startIndex-1], x[startIndex:length(x)])
+    }
 }
